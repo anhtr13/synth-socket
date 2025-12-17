bfe0c23667f90011add484c5c146e57edd62a46e
anhtr13 <anh.bigbro@gmail.com>
2025-12-17 21:20:29 +0700

    update nginx service

--- update_share.sh
+++ update_share.sh
@@ -5,9 +5,5 @@ sqlc generate
 targets=("./api/internal/" "./socket/internal/" "./cron/internal/")
 
 for target in ${targets[@]}; do
-  mkdir -p $target
-done
-
-for target in ${targets[@]}; do
   cp -r -f ./share/* $target
 done
